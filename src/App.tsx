6cbda1a816eef718d94fbb476703d5193ec6fa63
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2024-04-02 00:54:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My new title</h1>
+      <h1>{localStorage.getItem("newTitle") ? "My new title" : "React + Vite"}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
